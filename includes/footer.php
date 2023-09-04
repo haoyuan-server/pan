ea03951a27c72b4eea4befc5c54c4b1b02600374
haoyuan-server <143761561+haoyuan-server@users.noreply.github.com>
2023-09-04 20:27:17 +0800

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,22 @@
+</main>
+    <footer id="page-footer" class="bg-body-light">
+        <div class="content py-3">
+            <div class="row fs-sm">
+                <div class="col-sm-6 order-sm-2 py-1 text-center text-sm-end">
+                    <a class="fw-semibold" href="/" target="_blank"><?php echo $conf['tongji']?></a>
+                </div>
+                <div class="col-sm-6 order-sm-1 py-1 text-center text-sm-start">
+                    &copy; <span data-toggle="year-copy"></span> <a class="fw-semibold" href="" target="_blank"><?php echo $conf['title']?></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
+<script src="//cdn.staticfile.org/twitter-bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<script src="//cdn.staticfile.org/bootstrap-material-design/0.5.10/js/material.min.js"></script>
+<script src="//cdn.staticfile.org/bootstrap-material-design/0.5.10/js/ripples.min.js"></script>
+<script>
+    $.material.init();
+</script>
+</body>
+</html>
